8c940fc7915b8d2dab244ac49e6b2dac7c797d27
erickeira <erick.almeida.ricardo@gmail.com>
2024-07-22 20:37:54 -0400

    favicon

--- components/CustomHead.js
+++ components/CustomHead.js
@@ -11,6 +11,7 @@ export default function CustomHead({
             <title>{ title || 'Nerdola - administrativo'}</title>
             <meta name="description" content={ description || "" } />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href={favicon || "/logo_pequena.png" } />
             <meta property="og:image" content={ ogImage || "" }/>
   		    <meta name="robots" content="noindex, nofollow"/>
         </Head>
